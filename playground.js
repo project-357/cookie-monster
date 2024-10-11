f309158502a1b80c90bb833902a4d01d75dd8e0b
Christian Rudder <ringthebellpepper@gmail.com>
2024-10-11 11:58:27 -0400

    targeting under armor

--- playground.js
+++ playground.js
@@ -1,7 +1,18 @@
 const { fetchCookiesAndSaveToCSV: cookm } = require('./cookm');
 
-// const ua = 'https://www.underarmour.com/';
+const urls = [
+    "https://www.underarmour.com/",
+    "https://www.underarmour.com/en-us/c/shoes/",
+    "https://www.underarmour.com/en-us/p/basketball/unisex_ua_lockdown_7_basketball_shoes/3028512.html?dwvar_3028512_color=300",
+    "https://www.underarmour.com/en-us/p/basketball/",
+    "https://help.underarmour.com/s/",
+    "https://www.underarmour.com/en-us/cart/",
+    "https://www.underarmour.com/en-us/t/shop-app.html",
+    "https://www.underarmour.com/en-us/c/new-arrivals/ ",
+    "https://www.underarmour.com/en-us/c/mens/",
+]
+//  const ua = 'https://www.underarmour.com/';
 // cookm(ua);
 
-const gh = 'https://github.com/neezacoto/cookie-monster';
-cookm(gh);
+urls.forEach(url => cookm(url));
+
